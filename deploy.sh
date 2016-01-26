f3f637cefc88b43d09660645b0a8a7becae29fd2
Dylan <dylanslack@gmail.com>
2016-01-25 22:29:06 -0500

    Add branch switching check

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,18 @@ mkdir -p /tmp/restle-guides
 rm -rf /tmp/restle-guides
 cp -R ./guides/ /tmp/restle-guides
 git checkout gh-pages
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+echo "Current branch is $branch"
+
+if [ $branch=="master" ]; then
+  echo "Failed to swith to gh-pages"
+  exit
+fi
+
+echo "Successfully switched to gh-pages"
+
 cp -R /tmp/restle-guides/ ./
 git add --all
 git commit -m Deploy Restle Guides
