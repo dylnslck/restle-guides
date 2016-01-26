34b05f806c77bf55a9ea94f259a154c06436b18f
Dylan <dylanslack@gmail.com>
2016-01-25 22:20:55 -0500

    Add directory copying to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ echo "Building"
 echo "Deploying"
 mkdir -p /tmp/restle-guides
 rm -rf /tmp/restle-guides
-cp ./guides/* /tmp/restle-guides
+cp -R ./guides/ /tmp/restle-guides
 git checkout gh-pages
-cp /tmp/restle-guides/* ./
+cp -R /tmp/restle-guides/ ./
 git add --all
 git commit -m Deploy Restle Guides
 git push origin gh-pages
