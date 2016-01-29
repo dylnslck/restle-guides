d514e025f452e7aa3f107b16bbeac71d0ecf9719
Dylan <dylanslack@gmail.com>
2016-01-28 19:02:10 -0500

    Update 2016-01-29T00:02:06.168Z

--- codes/start.js
+++
@@ -1,9 +0,0 @@
-// Create a very basic application.
-const Restle = require('restle');
-
-const app = new Restle({
-  namespace: 'api',
-  port: 3000,
-});
-
-app.on('ready', () => console.log(`Restle is listening at port ${app.port}`));
